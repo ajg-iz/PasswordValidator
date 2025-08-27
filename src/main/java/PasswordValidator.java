db30ddf41f8d9b47220955115778d2100d0d53eb
ajg-iz <andreas.goebel@gmail.com>
2025-08-27 13:47:39 +0200

    Method- and test-stubs further defined

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,26 +1,60 @@
 public final class PasswordValidator {
+
     public static void main(String[] args) {
-        boolean valid = isValid(args[0]);
+        boolean valid = false;
+        if (!args[0].isEmpty()) {
+            valid = isValid(args[0]);
+        }
         System.out.println(String.valueOf(valid));
     }
 
     public static boolean isValid(String password) {
-        return false;
+        boolean validated = false;
+        String specialChars = "!\"§$%&/()=?²³¼½¬{[]}\\,;.:+-*'#~|<>^°";
+        int minLength = 8;
+        if (hasMinLength(password, minLength)
+                && containsDigit(password)
+                && containsUpperAndLower(password)
+                && !isCommonPassword(password)
+                && containsSpecialChar(password, specialChars)) {
+            validated = true;
+        }
+        return validated;
     }
     public static boolean hasMinLength(String password, int min) {
-        return false;
+        return password.length() >= min;
     }
     public static boolean containsDigit(String password) {
-        return false;
+        boolean foundDigit = false;
+        for (int i = 0; i < 10; i++) {
+            if (password.contains(String.valueOf(i))) {
+                foundDigit = true;
+            }
+        }
+        return foundDigit;
     }
     public static boolean containsUpperAndLower(String password) {
-        return false;
+        boolean hasBoth = false;
+        if (!password.toLowerCase().equals(password)
+                && !password.toUpperCase().equals(password)) {
+            hasBoth = true;
+        }
+        return hasBoth;
     }
     public static boolean isCommonPassword(String password) {
         return false;
     }
-    public static boolean containsSpecialChar(String password, String allowed) {
-        return false;
+    public static boolean containsSpecialChar(String password, String specialChars) {
+        boolean foundSpecialChar = false;
+        for (int i = 0; i < specialChars.length(); i++) {
+            for (int j = 0; j <password.length(); j++) {
+                if (specialChars.charAt(i) == password.charAt(j)) {
+                    foundSpecialChar = true;
+                    break;
+                }
+            }
+        }
+        return foundSpecialChar;
     }
 
 }
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -13,8 +13,18 @@ class PasswordValidatorTest {
     }
 
     @org.junit.jupiter.api.Test
+    void hasMinLength_shouldReturnFalse_ifGivenStringSmallerThen8() {
+        assertFalse(PasswordValidator.hasMinLength("Bla?2", 8));
+    }
+
+    @org.junit.jupiter.api.Test
     void containsDigit_shouldReturnTrue_ifStringContainsAtLeastOneDigit() {
         assertTrue(PasswordValidator.containsDigit("Blabla2?"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsDigit_shouldReturnFalse_ifStringContainsNoDigit() {
+        assertFalse(PasswordValidator.containsDigit("Blablabla?"));
     }
 
     @org.junit.jupiter.api.Test
@@ -23,12 +33,27 @@ class PasswordValidatorTest {
     }
 
     @org.junit.jupiter.api.Test
+    void containsUpperAndLower_shouldReturnFalse_ifStringDoesntContainsUpperAndLower() {
+        assertFalse(PasswordValidator.containsUpperAndLower("blabla2?"));
+    }
+
+    @org.junit.jupiter.api.Test
     void isCommonPassword_shouldReturnTrue_ifStringIsInListOfCommonPasswords() {
         assertTrue(PasswordValidator.isCommonPassword("Blabla?2"));
     }
 
     @org.junit.jupiter.api.Test
+    void isCommonPassword_shouldReturnFalse_ifStringIsNotInListOfCommonPasswords() {
+        assertFalse(PasswordValidator.isCommonPassword("password"));
+    }
+
+    @org.junit.jupiter.api.Test
     void containsSpecialChar_shouldReturnTrue_ifStringContainsAtLeastOneSpecialCharacter() {
         assertTrue(PasswordValidator.containsSpecialChar("BlaBla2?", "!\"§$%&/()=?²³¼½¬{[]}\\,;.:+-*'#~|<>^°"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsSpecialChar_shouldReturnFalse_ifStringDoesntContainsAtLeastOneSpecialCharacter() {
+        assertFalse(PasswordValidator.containsSpecialChar("BlaBla22", "!\"§$%&/()=?²³¼½¬{[]}\\,;.:+-*'#~|<>^°"));
     }
 }
