506fb78408a96cadf019d2e7a1af06a142b0eac7
ajg-iz <andreas.goebel@gmail.com>
2025-08-27 11:42:30 +0200

    Method- and test-stubs further defined

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,25 +1,26 @@
-public class PasswordValidator {
+public final class PasswordValidator {
     public static void main(String[] args) {
-        System.out.println("hallo");
+        boolean valid = isValid(args[0]);
+        System.out.println(String.valueOf(valid));
     }
 
     public static boolean isValid(String password) {
-        return true;
+        return false;
     }
     public static boolean hasMinLength(String password, int min) {
-        return true;
+        return false;
     }
     public static boolean containsDigit(String password) {
-        return true;
+        return false;
     }
     public static boolean containsUpperAndLower(String password) {
-        return true;
+        return false;
     }
     public static boolean isCommonPassword(String password) {
-        return true;
+        return false;
     }
     public static boolean containsSpecialChar(String password, String allowed) {
-        return true;
+        return false;
     }
 
 }
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -3,26 +3,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class PasswordValidatorTest {
 
     @org.junit.jupiter.api.Test
-    void isValid() {
+    void isValid_shouldReturnTrue_ifAllChecksReturnTrue() {
+        assertTrue(PasswordValidator.isValid("Blablabla?22"));
     }
 
     @org.junit.jupiter.api.Test
-    void hasMinLength() {
+    void hasMinLength_shouldReturnTrue_ifGivenStringLongerOrEqualTo8() {
+        assertTrue(PasswordValidator.hasMinLength("Blabla?2", 8));
     }
 
     @org.junit.jupiter.api.Test
-    void containsDigit() {
+    void containsDigit_shouldReturnTrue_ifStringContainsAtLeastOneDigit() {
+        assertTrue(PasswordValidator.containsDigit("Blabla2?"));
     }
 
     @org.junit.jupiter.api.Test
-    void containsUpperAndLower() {
+    void containsUpperAndLower_shouldReturnTrue_ifStringContainsUpperAndLower() {
+        assertTrue(PasswordValidator.containsUpperAndLower("Blabla2?"));
     }
 
     @org.junit.jupiter.api.Test
-    void isCommonPassword() {
+    void isCommonPassword_shouldReturnTrue_ifStringIsInListOfCommonPasswords() {
+        assertTrue(PasswordValidator.isCommonPassword("Blabla?2"));
     }
 
     @org.junit.jupiter.api.Test
-    void containsSpecialChar() {
+    void containsSpecialChar_shouldReturnTrue_ifStringContainsAtLeastOneSpecialCharacter() {
+        assertTrue(PasswordValidator.containsSpecialChar("BlaBla2?", "!\"§$%&/()=?²³¼½¬{[]}\\,;.:+-*'#~|<>^°"));
     }
 }
