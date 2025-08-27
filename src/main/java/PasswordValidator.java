afc596799e1369361d95537d37c970b2b5a2041d
ajg-iz <andreas.goebel@gmail.com>
2025-08-27 11:15:27 +0200

    Method- and test-stubs

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -2,4 +2,24 @@ public class PasswordValidator {
     public static void main(String[] args) {
         System.out.println("hallo");
     }
+
+    public static boolean isValid(String password) {
+        return true;
+    }
+    public static boolean hasMinLength(String password, int min) {
+        return true;
+    }
+    public static boolean containsDigit(String password) {
+        return true;
+    }
+    public static boolean containsUpperAndLower(String password) {
+        return true;
+    }
+    public static boolean isCommonPassword(String password) {
+        return true;
+    }
+    public static boolean containsSpecialChar(String password, String allowed) {
+        return true;
+    }
+
 }
---
+++ src/test/java/PasswordValidatorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @org.junit.jupiter.api.Test
+    void isValid() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasMinLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsDigit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsUpperAndLower() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isCommonPassword() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsSpecialChar() {
+    }
+}
