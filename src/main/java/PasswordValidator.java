cc43f190a975fd60647698f01c9c4d2419c9f5dc
ajg-iz <andreas.goebel@gmail.com>
2025-08-27 14:34:17 +0200

    added 2 more methods for isCommonPassword() to enable custom password-lists

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -15,6 +15,7 @@ public final class PasswordValidator {
         boolean validated = false;
         String specialChars = "!\"§$%&/()=?²³¼½¬{[]}\\,;.:+-*'#~|<>^°";
         int minLength = 8;
+
         if (hasMinLength(password, minLength)
                 && containsDigit(password)
                 && containsUpperAndLower(password)
@@ -24,9 +25,11 @@ public final class PasswordValidator {
         }
         return validated;
     }
+
     public static boolean hasMinLength(String password, int min) {
         return password.length() >= min;
     }
+
     public static boolean containsDigit(String password) {
         boolean foundDigit = false;
         for (int i = 0; i < 10; i++) {
@@ -37,6 +40,7 @@ public final class PasswordValidator {
         }
         return foundDigit;
     }
+
     public static boolean containsUpperAndLower(String password) {
         boolean hasBoth = false;
         if (!password.toLowerCase().equals(password)
@@ -45,14 +49,26 @@ public final class PasswordValidator {
         }
         return hasBoth;
     }
-    public static boolean isCommonPassword(String password) {
+
+    public static boolean isCommonPassword(String password, String[] commonPasswords) {
         boolean foundCommon = false;
-        List<String> mostCommon = Arrays.asList(new String[]{"qwerty","password","qwertyuiop","mynoob","18atcskd2w","1q2w3e4r","3rjs1la7qe","google","1q2w3e4r5t","123qwe","zxcvbnm","1q2w3e"});
+        List<String> mostCommon = Arrays.asList(commonPasswords);
         if (mostCommon.contains(password)) {
             foundCommon = true;
         }
         return foundCommon;
     }
+
+    public static boolean isCommonPassword(String password, String commonPasswords, String seperator) {
+        String[] commonPasswordsArray = commonPasswords.split(seperator);
+        return isCommonPassword(password, commonPasswordsArray);
+    }
+
+    public static boolean isCommonPassword(String password) {
+        String commonPasswords = "qwerty,password,qwertyuiop,mynoob,18atcskd2w,1q2w3e4r,3rjs1la7qe,google,1q2w3e4r5t,123qwe,zxcvbnm,1q2w3e";
+        return isCommonPassword(password, commonPasswords, ",");
+    }
+
     public static boolean containsSpecialChar(String password, String specialChars) {
         boolean foundSpecialChar = false;
         for (int i = 0; i < specialChars.length(); i++) {
